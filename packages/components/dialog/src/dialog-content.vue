3ea61f03b887ce30444b4ddb4d8772d55709dac0
pl <403802162@qq.com>
2024-01-15 16:14:57 +0800

    修复

--- packages/components/dialog/src/dialog-content.vue
+++ packages/components/dialog/src/dialog-content.vue
@@ -5,7 +5,7 @@
 <template>
   <!-- 弹窗关闭按钮 -->
   <button class="closeIconbtn" v-if="showClose" @click="$emit('close')">
-    <close-icon color="#909399" size="20" />
+    <close-icon color="#909399" size="20px" />
   </button>
   <!-- 头部 -->
   <header :class="[ns.e('header')]">
